4aee33288807d2a899e8d8e70d09f60018a77a69
rohithkolaspace <144136347+rohithkolaspace@users.noreply.github.com>
2024-01-03 02:30:45 -0500

    Add files via upload

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,17 @@
+package Patterns;
+public class Pattern4{
+    public static void main (String[] args){
+        pattern(5);
+    }
+    //Method to print the pattern
+    static void pattern(int n){
+        //No.of rows
+        for(int row = 0; row <= n; row++){
+            //No.of cols
+            for(int col = 0; col <= row; col++){
+                System.out.print((char)(65+row));
+            }
+            System.out.println();
+        }
+    }
+}
