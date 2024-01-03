cf0cfe92b90a5fc1d39d6107d8617124ddd06531
rohithkolaspace <144136347+rohithkolaspace@users.noreply.github.com>
2024-01-03 01:18:40 -0500

    Update Pattern1.java

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,1 +1,20 @@
-a
+package Patterns;
+
+public class Pattern1{
+    public static void main (String[] args){
+        pattern(5);
+    }
+
+    //Method to print the pattern
+    static void pattern(int n){
+
+        //No.of rows
+        for(int row = 0; row < 5; row++){
+            //No.of cols
+            for(int col = 0; col < 5; col++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
